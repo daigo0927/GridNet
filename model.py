de3eb632ce82c0c2db44af34d030a287eb929250
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2018-04-28 22:07:39 +0900

    inital commit

---
+++ model.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+
+from modules import LateralBlock, DownSamplingBlock, UpSamplingBlock
+
+class GridNet(nn.Module):
+    def __init__(self, n_ch_init, n_ch_final,
+                 n_row = 3, n_col = 6, n_chs = [32, 64, 96]):
+        super().__init__()
+
+        assert n_row == len(n_chs), 'should give num channels for each row (scale stream)'
+        self.n_row = n_row
+        self.n_col = n_col
+        self.n_chs = n_chs
+
+        self.lateral_init = LateralBlock(n_ch_init, n_chs[0])
+        self.lateral_blocks = [[LateralBlock(n_ch, n_ch) for _ in range(n_col-1)] for n_ch in n_chs]
+        self.down_blocks = [[DownSamplingBlock(ch_in, ch_out) \
+                             for _ in range(int(n_col/2))]
+                            for ch_in, ch_out in zip(n_chs[:-1], n_chs[1:])]
+        self.up_blocks = [[UpSamplingBlock(ch_in, ch_out) \
+                           for _ in range(int(n_col/2))]
+                          for ch_in, ch_out in zip(n_chs[1:], n_chs[:-1])]
+        self.lateral_final = LateralBlock(n_chs[0], n_ch_final)
+                                    
+    def forward(self, x):
+
+        outputs = [[0 for _ in range(self.n_col)] for _ in range(self.n_row)]
+        outputs[0][0] = self.lateral_init(x)
+        
+        for col in range(self.n_col):
+            # downsampling
+            if col < int(self.n_col/2):
+                for row in range(self.n_row-1):
+                    outputs[row+1][col] += self.down_blocks[row][col](outputs[row][col])
+            # upsamlping
+            else:
+                # row : descending order
+                for row in reversed(range(self.n_row-1)):
+                    outputs[row][col] += self.up_blocks[row][col-3](outputs[row+1][col])
+            if col < self.n_col-1:
+                for row in range(self.n_row):
+                    outputs[row][col+1] = self.lateral_blocks[row][col](outputs[row][col])
+
+        return self.lateral_final(outputs[0][-1])
---
+++ modules.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+
+class LateralBlock(nn.Module):
+
+    def __init__(self, ch_in, ch_out):
+        super().__init__()
+        self.f = nn.Sequential(
+            nn.PReLU(),
+            nn.Conv2d(ch_in, ch_out, kernel_size = 3, padding = 1),
+            nn.PReLU(),
+            nn.Conv2d(ch_out, ch_out, kernel_size = 3, padding = 1)
+        )
+        if ch_in != ch_out:
+            self.conv = nn.Conv2d(ch_in, ch_out, kernel_size = 3, padding = 1)
+
+    def forward(self, x):
+        fx = self.f(x)
+        if fx.shape[1] != x.shape[1]:
+            x = self.conv(x)
+            
+        return fx + x
+
+    
+class DownSamplingBlock(nn.Module):
+
+    def __init__(self, ch_in, ch_out):
+        super().__init__()
+        self.f = nn.Sequential(
+            nn.PReLU(),
+            nn.Conv2d(ch_in, ch_out, kernel_size = 3, stride = 2, padding = 1),
+            nn.PReLU(),
+            nn.Conv2d(ch_out, ch_out, kernel_size = 3, padding = 1)
+        )
+
+    def forward(self, x):
+        return self.f(x)
+
+class UpSamplingBlock(nn.Module):
+
+    def __init__(self, ch_in, ch_out):
+        super().__init__()
+        self.f = nn.Sequential(
+            nn.UpsamplingBilinear2d(scale_factor = 2),
+            nn.PReLU(),
+            nn.Conv2d(ch_in, ch_out, kernel_size = 3, padding = 1),
+            nn.PReLU(),
+            nn.Conv2d(ch_out, ch_out, kernel_size = 3, padding = 1)
+        )
+
+    def forward(self, x):
+        return self.f(x)
