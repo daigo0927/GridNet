1843ea698b16d8e1a838f4e9bb117c8347f38dd3
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2018-05-02 17:41:32 +0900

    add cofirmed versions

--- model.py
+++ model.py
@@ -4,55 +4,32 @@ import torch.nn as nn
 from modules import LateralBlock, DownSamplingBlock, UpSamplingBlock
 
 class GridNet(nn.Module):
-    def __init__(self, n_ch_init, n_ch_final,
-                 n_row = 3, n_col = 6, n_chs = [32, 64, 96]):
+    def __init__(self, in_chs, out_chs, grid_chs = [32, 64, 96]):
+                 # n_row = 3, n_col = 6, n_chs = [32, 64, 96]):
         super().__init__()
 
-        assert n_row == len(n_chs), 'should give num channels for each row (scale stream)'
-        self.n_row = n_row
-        self.n_col = n_col
-        self.n_chs = n_chs
+        self.n_row = 3
+        self.n_col = 6
+        self.n_chs = grid_chs
+        assert len(grid_chs) == self.n_row, 'should give num channels for each row (scale stream)'
 
-        self.lateral_init = LateralBlock(n_ch_init, n_chs[0])
+        self.lateral_init = LateralBlock(in_chs, self.n_chs[0])
         
-        self.lateral_blocks = [[None for _ in range(n_col-1)] for _ in range(n_row)]
-        for r, n_ch in enumerate(n_chs):
-            for c in range(n_col-1):
+        for r, n_ch in enumerate(self.n_chs):
+            for c in range(self.n_col-1):
                 setattr(self, f'lateral_{r}_{c}', LateralBlock(n_ch, n_ch))
-                self.lateral_blocks[r][c] = eval(f'self.lateral_{r}_{c}')
 
-        self.down_blocks = [[None for _ in range(int(n_col/2))] for _ in range(n_row-1)]
-        for r, (ch_in, ch_out) in enumerate(zip(n_chs[:-1], n_chs[1:])):
-            for c in range(int(n_col/2)):
-                setattr(self, f'down_{r}_{c}', DownSamplingBlock(ch_in, ch_out))
-                self.down_blocks[r][c] = eval(f'self.down_{r}_{c}')
+        for r, (in_ch, out_ch) in enumerate(zip(self.n_chs[:-1], self.n_chs[1:])):
+            for c in range(int(self.n_col/2)):
+                setattr(self, f'down_{r}_{c}', DownSamplingBlock(in_ch, out_ch))
 
-        self.up_blocks = [[None for _ in range(int(n_col/2))] for _ in range(n_row-1)]
-        for r, (ch_in, ch_out) in enumerate(zip(n_chs[1:], n_chs[:-1])):
-            for c in range(int(n_col/2)):
-                setattr(self, f'up_{r}_{c}', UpSamplingBlock(ch_in, ch_out))
-                self.up_blocks[r][c] = eval(f'self.up_{r}_{c}')
+        for r, (in_ch, out_ch) in enumerate(zip(self.n_chs[1:], self.n_chs[:-1])):
+            for c in range(int(self.n_col/2)):
+                setattr(self, f'up_{r}_{c}', UpSamplingBlock(in_ch, out_ch))
                 
-        self.lateral_final = LateralBlock(n_chs[0], n_ch_final)
+        self.lateral_final = LateralBlock(self.n_chs[0], out_chs)
                                     
     def forward(self, x):
-
-        # outputs = [[0 for _ in range(self.n_col)] for _ in range(self.n_row)]
-        # outputs[0][0] = self.lateral_init(x)
-        
-        # for col in range(self.n_col):
-        #     # downsampling
-        #     if col < int(self.n_col/2):
-        #         for row in range(self.n_row-1):
-        #             outputs[row+1][col] += self.down_blocks[row][col](outputs[row][col])
-        #     # upsamlping
-        #     else:
-        #         for row in reversed(range(self.n_row-1)): # row : descending order
-        #             outputs[row][col] += self.up_blocks[row][col-int(self.n_col/2)](outputs[row+1][col])
-        #     # lateral stream
-        #     if col < self.n_col-1:
-        #         for row in range(self.n_row):
-        #             outputs[row][col+1] = self.lateral_blocks[row][col](outputs[row][col])
 
         state_00 = self.lateral_init(x)
         state_10 = self.down_0_0(state_00)
@@ -79,26 +56,3 @@ class GridNet(nn.Module):
         state_05 = self.up_0_2(state_15) + self.lateral_0_4(state_04)
 
         return self.lateral_final(state_05)
-        
-        # return self.lateral_final(outputs[0][-1])
-
-    # def forwardall(self, x):
-
-    #     outputs = [[0 for _ in range(self.n_col)] for _ in range(self.n_row)]
-    #     outputs[0][0] = self.lateral_init(x)
-        
-    #     for col in range(self.n_col):
-    #         # downsampling
-    #         if col < int(self.n_col/2):
-    #             for row in range(self.n_row-1):
-    #                 outputs[row+1][col] += self.down_blocks[row][col](outputs[row][col])
-    #         # upsamlping
-    #         else:
-    #             for row in reversed(range(self.n_row-1)): # row : descending order
-    #                 outputs[row][col] += self.up_blocks[row][col-int(self.n_col/2)](outputs[row+1][col])
-    #         # lateral stream
-    #         if col < self.n_col-1:
-    #             for row in range(self.n_row):
-    #                 outputs[row][col+1] = self.lateral_blocks[row][col](outputs[row][col])
-
-    #     return outputs
